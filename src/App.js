4f3524a253781497ec74d49ffe02fe34d6b3249b
olegatorh <60426669+olegatorh@users.noreply.github.com>
2021-02-03 21:47:24 +0200

    first
    step

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoList from "./components/TodoList";
+import Form from "./components/FormControl";
+
+function App() {
+  return (
+    <div className="App">
+        <Form/>
+        <TodoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+
+const TodoList = () => {
+    return(
+        <div>
+            dwa
+        </div>
+    )
+}
+
+export default TodoList
